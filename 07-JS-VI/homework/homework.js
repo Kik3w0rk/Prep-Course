12d8939d2b20d6544da5b9d55545b2b512decd90
Henrique <kikework32@gmail.com>
2022-07-27 18:18:43 -0400

    tareas realizadas

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -7,18 +7,20 @@ function mayuscula(nombre) {
   var letra1=nombre[0].toUpperCase;
   var resto=nombre.slice(1);
   var respuesta=letra1+resto;
-  return respuesta
+  return respuesta;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -26,12 +28,20 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var suma=0;
+  for (let i = 0; i < numeros.length; i++) {
+    suma=suma+numeros[i];
+  }
+  cb(suma);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function(elemento){
+    cb(elemento);
+  })
 }
 
 function map(array, cb) {
@@ -39,12 +49,20 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var arreglo = array.map(function(elemento){
+    return cb(elemento);
+  })
+  return arreglo;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var arreglo=array.filter(function(letra){
+    return letra[0]==="a"
+  })
+  return arreglo;
 }
 
 // No modificar nada debajo de esta línea
